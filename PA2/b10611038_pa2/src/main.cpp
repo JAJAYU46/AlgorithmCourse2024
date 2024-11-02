3a7066907fd72c5e585537006f9ccb25092f2ada
JAJAYU46 <jajajaja4646@gmail.com>
2024-11-03 04:06:27 +0800

    Check point after cleaning code

--- PA2/b10611038_pa2/src/main.cpp
+++ PA2/b10611038_pa2/src/main.cpp
@@ -13,16 +13,7 @@ using namespace std;
 #include "sort_tool.h"
 // #include "sort_tool.cpp" //如果沒有用makefile, 分開寫記得兩個都要include!
 
-void help_message() {
-    ////cout << "usage: NTU_sort -[IS|MS|BMS|QS|RQS|HS] <input_file> <output_file>" << endl;
-    ////cout << "options:" << endl;
-    ////cout << "   IS  - Insersion Sort" << endl;
-    ////cout << "   MS  - Merge Sort" << endl;
-    ////cout << "   BMS - Bottom-up Merge Sort" << endl;
-    ////cout << "   QS  - Quick Sort" << endl;
-    ////cout << "   RQS - Randomized Quick Sort" << endl;
-    ////cout << "   HS  - Heap Sort" << endl;
-}
+
 void printVector(const std::vector<int>& vec) {
     for (int value : vec) {
         //cout << value << " ";
@@ -31,20 +22,18 @@ void printVector(const std::vector<int>& vec) {
 }
 
 int main(int argc, char *argv[]) {
-    // help_message();
+    
     if (argc != 3) {
-        ////cout<<"haha";
-        // std::cerr << "Usage: " << argv[0] << " <input file> <output file>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <input file> <output file>" << std::endl;
         return 1;
     }
-    ////cout << "   aaa" << endl;
-    // help_message();
+
     ////cout<<"lala"<< endl;
     //////////// read the input file /////////////
     //This creates an fstream object and then opens the file in output mode.
     //ofstream: ofstream is specifically designed for output (writing) only. It simplifies the interface if you only need to write to the file, as it doesn’t support reading operations.
     //fstream: fstream is a more general file stream class that supports both input (ios::in) and output (ios::out) operations.
-    // char buffer[200];
+
     int n2,n;
     ////cout<<"ok1"<< endl;
     fstream fin(argv[1]);
@@ -52,16 +41,10 @@ int main(int argc, char *argv[]) {
     fout.open(argv[2],ios::out);
     fin>> n2; //裝2n個點
     n=n2/2;
-    ////cout<<"n2: "<<n2<< endl;
-    ////cout<<"ok2"<< endl;
-    //fin.getline(array,1); //reads up to 200 characters from the file stream fin and stores them in the buffer array.
-    //fin.getline(buffer,200); //stops at the newline.
+
     int a,b;
     // vector<int> data_a, data_b; //紀錄個chord中兩端點
 
-
-
-    // std::vector<std::pair<int, int>> chords;
     // int chords[2*n]; //建立pair array, 之後會索引紀錄chord(為chord[i] = 0~2n-1個端點各自chord的另一端) 
     std::vector<int> chords(2*n);  //先給各個vector一個已知的空間, 之後才可以確定chords[a]=b不會出錯
 
@@ -80,12 +63,6 @@ int main(int argc, char *argv[]) {
                     // data[1] will be the second data and so on.
     }
 
-    // Print the vector
-    ////cout<<"chords: "<< endl;
-
-    // printVector(chords);
-    
-    //cout<<"ok3"<< endl;
     ///////////////Maximum Planner Part////////////////
     MaxPlanarSubset mps(n, chords);
     //cout<<"ok6"<< endl;
@@ -106,33 +83,6 @@ int main(int argc, char *argv[]) {
     }
     fin.close();
     fout.close();
-    
-    //auto result = mps.get_maximumPlanarSubset_chord();
-
-    // std::ifstream input(argv[1]);
-    // std::ofstream output(argv[2]);
-
-    // if (!input || !output) {
-    //     std::cerr << "Error opening file!" << std::endl;
-    //     return 1;
-    // }
-
-    // int n;
-    // input >> n; //Reads only the next word or number, stopping at whitespace
-    // std::vector<std::pair<int, int>> chords;
-
-    // int a, b;
-    // while (input >> a >> b && (a || b)) {
-    //     chords.emplace_back(a, b);
-    // }
-
-    // MaxPlanarSubset mps(n, chords);
-    // auto result = mps.computeMPS();
-
-    // output << result.size() << std::endl;
-    // for (const auto &chord : result) {
-    //     output << chord.first << " " << chord.second << std::endl;
-    // }
 
     return 0;
 }
